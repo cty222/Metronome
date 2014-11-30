0e5c605bd1684f1fbf91029d73e18d7dd7638b4e
C-ty <cty@ctyde-macbook.local>
2014-11-30 16:50:03 +0800

    Add Picture1.0

--- Groove/MetronomeBottomSubViewIphone.m
+++ Groove/MetronomeBottomSubViewIphone.m
@@ -28,27 +28,29 @@
         self.SelectGrooveBar = [[MetronomeSelectBar alloc] initWithFrame:self.SelectGrooveBar.frame];
         [self addSubview:self.SelectGrooveBar];
         
-        // Back ground
-        self.backgroundColor = [UIColor whiteColor];
-       
         [self.AccentCircleVolumeButton removeFromSuperview];
         self.AccentCircleVolumeButton = [[CircleButton alloc] initWithFrame:self.AccentCircleVolumeButton.frame];
+        self.AccentCircleVolumeButton.SignPicture.image = [UIImage imageNamed:@"AccentNote"];
         [self addSubview:self.AccentCircleVolumeButton];
 
         [self.QuarterCircleVolumeButton removeFromSuperview];
         self.QuarterCircleVolumeButton = [[CircleButton alloc] initWithFrame:self.QuarterCircleVolumeButton.frame];
+        self.QuarterCircleVolumeButton.SignPicture.image = [UIImage imageNamed:@"QuarterNote"];
         [self addSubview:self.QuarterCircleVolumeButton];
 
         [self.EighthNoteCircleVolumeButton removeFromSuperview];
         self.EighthNoteCircleVolumeButton = [[CircleButton alloc] initWithFrame:self.EighthNoteCircleVolumeButton.frame];
+        self.EighthNoteCircleVolumeButton.SignPicture.image = [UIImage imageNamed:@"8Note"];
         [self addSubview:self.EighthNoteCircleVolumeButton];
 
         [self.SixteenthNoteCircleVolumeButton removeFromSuperview];
         self.SixteenthNoteCircleVolumeButton = [[CircleButton alloc] initWithFrame:self.SixteenthNoteCircleVolumeButton.frame];
+        self.SixteenthNoteCircleVolumeButton.SignPicture.image = [UIImage imageNamed:@"16Note"];
         [self addSubview:self.SixteenthNoteCircleVolumeButton];
 
         [self.TrippleNoteCircleVolumeButton removeFromSuperview];
         self.TrippleNoteCircleVolumeButton = [[CircleButton alloc] initWithFrame:self.TrippleNoteCircleVolumeButton.frame];
+        self.TrippleNoteCircleVolumeButton.SignPicture.image = [UIImage imageNamed:@"TrippleNote"];
         [self addSubview:self.TrippleNoteCircleVolumeButton];
        
     }
