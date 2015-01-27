75d139dfcbab6840bcd51367f03696b66815e1a8
C-ty <cty@ctyde-macbook.local>
2015-01-28 01:30:00 +0800

    Add Empty volume sets control

---
+++ Groove/Metronome/Controller/ToolClass/VolumeSetsControl.h
@@ -0,0 +1,13 @@
+//
+//  VolumeSetsControl.h
+//  Groove
+//
+//  Created by C-ty on 2015/1/28.
+//  Copyright (c) 2015年 Cty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VolumeSetsControl : NSObject
+
+@end
---
+++ Groove/Metronome/Controller/ToolClass/VolumeSetsControl.m
@@ -0,0 +1,13 @@
+//
+//  VolumeSetsControl.m
+//  Groove
+//
+//  Created by C-ty on 2015/1/28.
+//  Copyright (c) 2015年 Cty. All rights reserved.
+//
+
+#import "VolumeSetsControl.h"
+
+@implementation VolumeSetsControl
+
+@end
