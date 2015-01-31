b06f6b709c01d8b6f7e5aedec6ffcc2b053e9dc8
C-ty <cty@ctyde-macbook.local>
2015-02-01 01:04:22 +0800

    update simplified Chinese string

--- Groove/Tools/LocalizedMutilanguage.m
+++ Groove/Tools/LocalizedMutilanguage.m
@@ -23,6 +23,15 @@ static NSBundle *DefaultLanguageBundle = nil;
         if (DefaultLanguageBundle == nil)
         {
             NSString *DefaultLanguage = @"en";
+            if ([[Language substringToIndex:[@"zh" length]]isEqualToString:@"zh"]) {
+                if ([[Language substringToIndex:[@"zh-Hant" length]]isEqualToString:@"zh-Hant"]) {
+                    DefaultLanguage = @"zh-Hant";
+                }
+                else
+                {
+                    DefaultLanguage = @"zh-Hans";
+                }
+            }
             NSString *DefaultBundlePath = [[NSBundle mainBundle] pathForResource:DefaultLanguage ofType:@"lproj"];
             DefaultLanguageBundle = [NSBundle bundleWithPath:DefaultBundlePath];
         }
